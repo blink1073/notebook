f5c741ac48d9a183d3cc90c636176990e387a15d
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-06-11 17:25:54 -0700

    [comm] make it typescript

--- notebook/static/services/kernels/comm.ts
+++ notebook/static/services/kernels/comm.ts
@@ -1,25 +1,29 @@
 // Copyright (c) Jupyter Development Team.
 // Distributed under the terms of the Modified BSD License.
 
-define([
-    'jquery',
-    'base/js/utils',
-], function($, utils) {
-    "use strict";
+
+import $ = require('jquery')
+import utils = require('base/js/utils') 
+
+export class CommManager {
+
+    comms:any
+    targets
+    kernel
 
     //-----------------------------------------------------------------------
     // CommManager class
     //-----------------------------------------------------------------------
     
-    var CommManager = function (kernel) {
+    constructor(kernel) {
         this.comms = {};
         this.targets = {};
         if (kernel !== undefined) {
             this.init_kernel(kernel);
         }
-    };
+    }
     
-    CommManager.prototype.init_kernel = function (kernel) {
+    public init_kernel(kernel) {
         /**
          * connect the kernel, and register message handlers
          */
@@ -29,9 +33,9 @@ define([
             var msg_type = msg_types[i];
             kernel.register_iopub_handler(msg_type, $.proxy(this[msg_type], this));
         }
-    };
+    }
     
-    CommManager.prototype.new_comm = function (target_name, data, callbacks, metadata) {
+    public new_comm(target_name, data, callbacks, metadata):Comm {
         /**
          * Create a new Comm, register it, and open its Kernel-side counterpart
          * Mimics the auto-registration in `Comm.__init__` in the Jupyter Comm
@@ -40,47 +44,47 @@ define([
         this.register_comm(comm);
         comm.open(data, callbacks, metadata);
         return comm;
-    };
+    }
     
-    CommManager.prototype.register_target = function (target_name, f) {
+    public register_target(target_name, f) {
         /**
          * Register a target function for a given target name
          */
         this.targets[target_name] = f;
-    };
+    }
     
-    CommManager.prototype.unregister_target = function (target_name, f) {
+    public unregister_target(target_name, f) {
         /**
          * Unregister a target function for a given target name
          */
         delete this.targets[target_name];
-    };
+    }
     
-    CommManager.prototype.register_comm = function (comm) {
+    public register_comm(comm:Comm) {
         /**
          * Register a comm in the mapping
          */
         this.comms[comm.comm_id] = Promise.resolve(comm);
         comm.kernel = this.kernel;
         return comm.comm_id;
-    };
+    }
     
-    CommManager.prototype.unregister_comm = function (comm) {
+    public unregister_comm(comm:Comm):void {
         /**
          * Remove a comm from the mapping
          */
         delete this.comms[comm.comm_id];
-    };
+    }
     
     // comm message handlers
     
-    CommManager.prototype.comm_open = function (msg) {
+    public comm_open(msg):Comm {
         var content = msg.content;
         var that = this;
         var comm_id = content.comm_id;
 
         this.comms[comm_id] = utils.load_class(content.target_name, content.target_module, 
-            this.targets).then(function(target) {
+            this.targets).then(function(target:(a:any,b:any)=> any) {
                 var comm = new Comm(content.target_name, comm_id);
                 comm.kernel = that.kernel;
                 try {
@@ -97,9 +101,9 @@ define([
                 return Promise.resolve(response).then(function() {return comm;});
             }, utils.reject('Could not open comm', true));
         return this.comms[comm_id];
-    };
+    }
     
-    CommManager.prototype.comm_close = function(msg) {
+    public comm_close(msg) {
         var content = msg.content;
         if (this.comms[content.comm_id] === undefined) {
             console.error('Comm promise not found for comm id ' + content.comm_id);
@@ -117,9 +121,9 @@ define([
             // get an undefined comm input
         });
         return this.comms[content.comm_id];
-    };
+    }
     
-    CommManager.prototype.comm_msg = function(msg) {
+    public comm_msg(msg) {
         var content = msg.content;
         if (this.comms[content.comm_id] === undefined) {
             console.error('Comm promise not found for comm id ' + content.comm_id);
@@ -135,60 +139,69 @@ define([
             return comm;
         });
         return this.comms[content.comm_id];
-    };
+    }
     
     //-----------------------------------------------------------------------
     // Comm base class
     //-----------------------------------------------------------------------
+}
+
+export class Comm {
+
+    kernel
+    target_name
+    comm_id
+    _msg_callback
+    _close_callback
     
-    var Comm = function (target_name, comm_id) {
+    constructor(target_name, comm_id?) {
         this.target_name = target_name;
         this.comm_id = comm_id || utils.uuid();
         this._msg_callback = this._close_callback = null;
-    };
+    }
     
     // methods for sending messages
-    Comm.prototype.open = function (data, callbacks, metadata) {
+    public open(data, callbacks, metadata) {
         var content = {
             comm_id : this.comm_id,
             target_name : this.target_name,
             data : data || {},
         };
         return this.kernel.send_shell_message("comm_open", content, callbacks, metadata);
-    };
+    }
     
-    Comm.prototype.send = function (data, callbacks, metadata, buffers) {
+    public send(data, callbacks, metadata, buffers) {
         var content = {
             comm_id : this.comm_id,
             data : data || {},
         };
         return this.kernel.send_shell_message("comm_msg", content, callbacks, metadata, buffers);
-    };
+    }
     
-    Comm.prototype.close = function (data, callbacks, metadata) {
+    public close(data?, callbacks?, metadata?) {
         var content = {
             comm_id : this.comm_id,
             data : data || {},
         };
         return this.kernel.send_shell_message("comm_close", content, callbacks, metadata);
-    };
+    }
     
     // methods for registering callbacks for incoming messages
-    Comm.prototype._register_callback = function (key, callback) {
+    private _register_callback(key, callback) {
         this['_' + key + '_callback'] = callback;
-    };
+    }
     
-    Comm.prototype.on_msg = function (callback) {
+    public on_msg(callback) {
         this._register_callback('msg', callback);
-    };
+    }
     
-    Comm.prototype.on_close = function (callback) {
+    public on_close(callback) {
         this._register_callback('close', callback);
-    };
+    }
     
     // methods for handling incoming messages
     
-    Comm.prototype._callback = function (key, msg) {
+    private _callback(key, msg) {
         var callback = this['_' + key + '_callback'];
         if (callback) {
             try {
@@ -197,18 +210,14 @@ define([
                 console.log("Exception in Comm callback", e, e.stack, msg);
             }
         }
-    };
+    }
     
-    Comm.prototype.handle_msg = function (msg) {
+    public handle_msg(msg) {
         this._callback('msg', msg);
-    };
+    }
     
-    Comm.prototype.handle_close = function (msg) {
+    public handle_close(msg) {
         this._callback('close', msg);
-    };
+    }
     
-    return {
-        'CommManager': CommManager,
-        'Comm': Comm
-    };
-});
+}
